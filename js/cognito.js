093004e2617318db8e5469d7204de211492d2ea2
Selva-AK <selvaak008@gmail.com>
2024-05-29 13:10:04 +0530

    loading screen added

--- js/cognito.js
+++ js/cognito.js
@@ -5,6 +5,9 @@ document.querySelector('#form').addEventListener('submit', (e) => {
     const phone_number = document.querySelector('#number')
 
     // Calling the signIN function
+    // Displaying loading screen
+    document.querySelector('#loading-wrapper').classList.remove('d-none')
+
     signIn(phone_number.value)
 })
 
@@ -26,14 +29,33 @@ function signIn(phone_number) {
     fetch("https://xo6m7i8hlg.execute-api.ap-south-1.amazonaws.com/dev/cognito-custom-auth/initiate-auth", requestOptions)
         .then((response) => response.json())
         .then((result) => {
+            // Removing loading screen after getting result
+            document.querySelector('#loading-wrapper').classList.add('d-none')
+
             console.log(result.body)
-            parsedBody = JSON.parse(result.body)
-            sessionStorage.setItem('session',parsedBody.Session)
-            sessionStorage.setItem('username', phone_number)
-            // Hiding the mobile number form and reveal otp form
-            document.getElementById('sign-in').style.display = 'none'
-            document.getElementById('verify-otp').style.display = 'block'
-            document.querySelector('#mobile_number_placeholder').innerHTML = phone_number
+            if(result.statusCode == 200){
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: 'OTP successfully sent to your mobile number.'
+                })
+
+                parsedBody = JSON.parse(result.body)
+                sessionStorage.setItem('session',parsedBody.Session)
+                sessionStorage.setItem('username', phone_number)
+                // Hiding the mobile number form and reveal otp form
+                document.getElementById('sign-in').style.display = 'none'
+                document.getElementById('verify-otp').style.display = 'block'
+                document.querySelector('#mobile_number_placeholder').innerHTML = phone_number
+            }
+            else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops!',
+                    text: 'Something went wrong',
+                    footer: `<div class="alert alert-danger">${result.body}</div>`
+                })
+            }
         })
         .catch((error) => console.error(error))
 }
@@ -44,6 +66,9 @@ document.querySelector('#otpForm').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const OTP = document.querySelector('#OTPnumber')
+
+    // Displaying loading screen
+    document.querySelector('#loading-wrapper').classList.remove('d-none')
     verifyOTP(OTP.value)
 })
 
@@ -67,12 +92,15 @@ function verifyOTP(OTP){
     fetch("https://xo6m7i8hlg.execute-api.ap-south-1.amazonaws.com/dev/cognito-custom-auth/respond-to-auth-challenge", requestOptions)
         .then((response) => response.json())
         .then((result) => {
+            // Remove loading screen
+            document.querySelector('#loading-wrapper').classList.add('d-none')
             console.log(result)
             if(result.statusCode == 200){
                 parsedBody = JSON.parse(result.body)
                 console.log(parsedBody.AuthenticationResult)
                 sessionStorage.setItem('id_token', parsedBody.AuthenticationResult.IdToken)
                 sessionStorage.setItem('access_token', parsedBody.AuthenticationResult.AccessToken)
+                location.href = `https://dailykural.in/searchblogs.html#id_token=${parsedBody.AuthenticationResult.IdToken}&access_token=${parsedBody.AuthenticationResult.AccessToken}`
             }
         })
         .catch((error) => console.error(error))
