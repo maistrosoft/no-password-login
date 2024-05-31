4aaa7d6a42a505b1b358e176b3e5453c323844f4
Selva-AK <selvaak008@gmail.com>
2024-05-31 18:37:23 +0530

    phone number login page UI changed

--- js/cognito.js
+++ js/cognito.js
@@ -13,6 +13,19 @@ document.querySelector('#form').addEventListener('submit', (e) => {
     }
 })
 
+ // Define the event listener function
+ function handleBeforeUnload(e) {
+    // Prevent the default action of the event
+    e.preventDefault();
+    
+    // For some browsers, e.returnValue needs to be set
+    e.returnValue = '';
+
+    // Return a value for older browsers
+    return '';
+}
+
+
 function signIn(phone_number) {
     // Displaying loading screen
     document.querySelector('#loading-wrapper').classList.remove('d-none')
@@ -48,19 +61,12 @@ function signIn(phone_number) {
                 sessionStorage.setItem('session',parsedBody.Session)
                 sessionStorage.setItem('username', phone_number)
                 // Hiding the mobile number form and reveal otp form
-                document.getElementById('sign-in').style.display = 'none'
-                document.getElementById('verify-otp').style.display = 'block'
+                document.getElementById('sign-in').classList.add('d-none')
+                document.getElementById('verify-otp').classList.remove('d-none')
                 document.querySelector('#mobile_number_placeholder').innerHTML = phone_number
-                window.addEventListener('beforeunload',  (e) => {
-                    // Prevent the default action of the event
-                    e.preventDefault()
-                
-                    // For some browsers, e.returnValue needs to be set
-                    e.returnValue = ''
-                
-                    // Return a value for older browsers
-                    return ''
-                })
+
+                // Add the event listener
+                window.addEventListener('beforeunload', handleBeforeUnload)
                 
             }
             else {
@@ -76,15 +82,17 @@ function signIn(phone_number) {
 }
 
 // For OTP form
-document.querySelector('#otpForm').addEventListener('submit', (e) => {
-    // preventing the submit event
-    e.preventDefault()
+document.querySelector('#otpBtn').addEventListener('click', (e) => {
 
-    const OTP = document.querySelector('#OTPnumber')
-
+    const inputs = document.querySelectorAll('.otp-field input')
+    let otp = ''
+    inputs.forEach(input => {
+        otp += input.value
+    })
+    console.log(`Entered OTP: ${otp}`)
     // Displaying loading screen
     document.querySelector('#loading-wrapper').classList.remove('d-none')
-    verifyOTP(OTP.value)
+    verifyOTP(otp)
 })
 
 function verifyOTP(OTP){
@@ -115,6 +123,9 @@ function verifyOTP(OTP){
                     parsedBody = JSON.parse(result.body)
                     sessionStorage.setItem('id_token', parsedBody.AuthenticationResult.IdToken)
                     sessionStorage.setItem('access_token', parsedBody.AuthenticationResult.AccessToken)
+                    
+                    // Removing the eventListener
+                    window.removeEventListener('beforeunload', handleBeforeUnload)
                     location.href = `https://dailykural.in/searchblogs.html#id_token=${parsedBody.AuthenticationResult.IdToken}&access_token=${parsedBody.AuthenticationResult.AccessToken}`
                 }
                 catch(error){
